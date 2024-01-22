9062e618ea2f35789aba0ffc30508e470ac659c2
Igor-Xavier <igor.oliveiraxavier93@gmail.com>
2024-01-22 13:20:27 -0300

    Aprendendo native

---
+++ src/components/Form/index.js
@@ -0,0 +1,54 @@
+import {React, useState} from "react";
+import {Text, TextInput, View, Button} from "react-native"
+import ResultImc from "./ResultImc";
+export default function Form(){
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("Preencha o peso e altura")
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+
+
+function imcCalculator(){
+    return setImc((weight/(height*height)).toFixed(2))
+}
+
+function validationImc(){
+    if(height !== null && weight !== null){
+        imcCalculator()
+        setHeight(null)
+        setWeight(null)
+        setMessageImc("Seu imc é igual a: ")
+        setTextButton("Calcular novamente")
+        return
+    }
+    setImc(null)
+    setTextButton("Calcular")
+    setMessageImc("Preencha o peso e altura")
+
+}
+
+    return(
+        <View>
+            <View>
+                <Text>Altura:</Text>
+                <TextInput
+                    onChangeText={setHeight}
+                    value={height}
+                    placeholder="Ex: 1.75"
+                    keyboardType="numeric"
+                />
+                <Text>Peso:</Text>
+                <TextInput
+                    onChangeText={setWeight}
+                    value={weight}
+                    placeholder="Ex: 80.250"
+                    keyboardType="numeric"   
+                />
+                <Button title={textButton} onPress={()=>validationImc()}/>
+            </View>
+            <ResultImc messageResultImc={messageImc} resultImc={imc}/>
+        </View>
+    )
+}
